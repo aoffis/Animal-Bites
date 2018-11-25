3b8be5a200402dc4c826870962ee14b5792b1999
Ason Uthatham <Aloha_sonza@hotmail.com>
2018-11-25 12:36:00 +0700

    Analyzsis Dog

--- analysis.py
+++ analysis.py
@@ -13,4 +13,20 @@ def main():
     data_group = data.groupby(['SpeciesIDDesc']).size()
     print(data_group)
 
+    #Species of dog
+    dog_specie = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['BreedIDDesc']).size()
+    print(dog_specie)
+
+    #Gender of dog
+    dog_gen = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['GenderIDDesc']).size()
+    print(dog_gen)
+
+    #Color of dog
+    dog_color = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['color']).size()
+    print("---Color of Dog---", dog_color, sep="/n")
+
+    #color of Pitbull
+    pit_color = data[data['BreedIDDesc'] == 'PIT BULL'].groupby(['color']).size()
+    print("---Color of Pit bull---", pit_color, sep="/n")
+
 main()

b9ae07f87106fd420944203d783050b65ac3f80a
Ason Uthatham <Aloha_sonza@hotmail.com>
2018-11-26 13:56:45 +0700

    Sort file

--- analysis.py
+++ analysis.py
@@ -8,14 +8,15 @@ def main():
     """ Get data from CSV and mannage data """
     #Mannage File
     data = pandas.read_csv('Health_AnimalBites.csv')
+    print(data)
 
     #Clean data
-    data_group = data.groupby(['SpeciesIDDesc']).size()
+    data_group = data['SpeciesIDDesc'].value_counts()
     print(data_group)
 
     #Species of dog
-    dog_specie = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['BreedIDDesc']).size()
-    print(dog_specie)
+    dog_specie = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['BreedIDDesc'])
+    print(dog_specie.size().sort_values(ascending=False))
 
     #Gender of dog
     dog_gen = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['GenderIDDesc']).size()
@@ -23,10 +24,10 @@ def main():
 
     #Color of dog
     dog_color = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['color']).size()
-    print("---Color of Dog---", dog_color, sep="/n")
+    print("---Color of Dog---", dog_color.sort_values(ascending=False), sep="/n")
 
     #color of Pitbull
     pit_color = data[data['BreedIDDesc'] == 'PIT BULL'].groupby(['color']).size()
-    print("---Color of Pit bull---", pit_color, sep="/n")
+    print("---Color of Pit bull---", pit_color.sort_values(ascending=False), sep="/n")
 
 main()

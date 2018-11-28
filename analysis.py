01fd20abf6f816def4c0200c88c9bc6fbd382dd7
Thuchpun Apivitcholachat <thuchpunapivitcholachat@gmail.com>
2018-11-28 23:18:45 +0700

    Change text in .py

--- analysis.py
+++ analysis.py
@@ -16,23 +16,23 @@ def main():
     print(data_group.to_dict())
 
     #Species of dog
-    dog_specie = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['BreedIDDesc']).size().sort_values(ascending=False)
+    dog_specie = data[data['SpeciesIDDesc'] == 'Dog'].groupby(['BreedIDDesc']).size().sort_values(ascending=False)
     print(dog_specie.to_dict())
 
     #Gender of dog
-    dog_gen = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['GenderIDDesc']).size()
+    dog_gen = data[data['SpeciesIDDesc'] == 'Dog'].groupby(['GenderIDDesc']).size()
     print(dog_gen.to_dict())
 
     #Color of dog
-    dog_color = data[data['SpeciesIDDesc'] == 'DOG'].groupby(['color']).size()
+    dog_color = data[data['SpeciesIDDesc'] == 'Dog'].groupby(['color']).size()
     print("---Color of Dog---", dog_color.sort_values(ascending=False).to_dict(), sep="/n")
 
     #color of Pitbull
-    pit_color = data[data['BreedIDDesc'] == 'PIT BULL'].groupby(['color']).size()
+    pit_color = data[data['BreedIDDesc'] == 'Pit Bull'].groupby(['color']).size()
     print("---Color of Pit bull---", pit_color.sort_values(ascending=False).to_dict(), sep="/n")
 
     #bite_date >>> Pitbull
-    date = data[data['BreedIDDesc'] == 'PIT BULL'].groupby(['bite_date']).size()
+    date = data[data['BreedIDDesc'] == 'Pit Bull'].groupby(['bite_date']).size()
     print("---Bite date of Pit bull---", date, sep='/n')
 
     #pygal >>> build bar chart

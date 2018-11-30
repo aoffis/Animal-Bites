e453267744b89150c263e36f1a3cf4fcad95dfa2
aoffis <44945111+aoffis@users.noreply.github.com>
2018-11-30 15:25:23 +0700

    Really Final

--- analysis.py
+++ analysis.py
@@ -53,28 +53,49 @@ def main():
     date = data[data['BreedIDDesc'] == 'Pit Bull'].groupby(['bite_date']).size()
     print("---Bite date of Pit bull---", date, sep='/n')
 
-    #Show data in graph(by using pygal)
+
+
+    """
+    Show data in graph(by using pygal)
+    """
+
     #Show most animal that bite people in U.S.A.(in pie chart)
     animal = data_group.sort_values(ascending=False).to_dict()
-    pie = pygal.Pie()
+    sol = pygal.SolidGauge(half_pie=True, inner_radius=0.7)
     for i in animal:
-        pie.add(i, animal[i])
-    pie.title = 'Most Animal that Bite People'
-    pie.render_to_file('wowpie.svg')
-    pie.render_in_browser()
-    
-    #Show color of Pitbull(in bar chart)
-    color_dog = pit_color.sort_values(ascending=False).to_dict()
+        sol.add(i, animal[i])
+    sol.title = 'Most Animal that Bite People'
+    sol.render_to_file('animals.svg')
+    sol.render_in_browser()
 
-    treemap = pygal.Treemap()
-    treemap.title = 'Most color of Pitbull'
+    #Show top 10 species dog(in treemap chart)
+    animalz = dog_specie.sort_values(ascending=False).to_dict()
+    tree = pygal.Treemap()
+    for i in animalz:
+        tree.add(i, animalz[i])
+    tree.title = 'Species of Dogs that Bite People'
+    tree.render_to_file('speciesdog.svg')
+    tree.render_in_browser()
+
+    #Show color of Dog(in horizontal bar chart)
+    color_dog = dog_color.sort_values(ascending=False)[:10].to_dict()
+    bar = pygal.HorizontalBar()
+    bar.title = 'Top 10 colors of Dog'
     for i in color_dog:
-        treemap.add(i, color_dog[i])
-    treemap.render_to_file('wowtreemap.svg')
-    treemap.render_in_browser()
+        bar.add(i, color_dog[i])
+    bar.render_to_file('top10colorsdog.svg')
+    bar.render_in_browser()
+    
+    #Show color of Pitbull(in horizontal bar chart)
+    color_pit = pit_color.sort_values(ascending=False)[:10].to_dict()
+    bar_ = pygal.HorizontalBar()
+    bar_.title = 'Top 10 colors of Pitbull'
+    for i in color_pit:
+        bar_.add(i, color_pit[i])
+    bar_.render_to_file('top10colorspitbull.svg')
+    bar_.render_in_browser()
 
     #Area
     zip_code = data[data['SpeciesIDDesc'] == 'Dog'].groupby(['victim_zip']).size().sort_values(ascending=False)
-    print(zip_code)
 
 main()
